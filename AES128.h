57acf881d522ad3339d5cb78187d312f0597b431
Kim Minwoo <43179854+davidkmw0810@users.noreply.github.com>
2019-10-13 18:11:15 +0900

    update AES

--- AES128.h
+++ AES128.h
@@ -8,3 +8,9 @@ typedef unsigned char BYTE;
 
 // 128비트 AES 암복호화 인터페이스
 void AES128(BYTE *plain, BYTE *cipher, BYTE *key, int mode);
+
+void expandKey(BYTE *key, BYTE *roundKey);
+BYTE* subBytes(BYTE *block, int mode);
+BYTE* shiftRows(BYTE *block, int mode);
+BYTE* mixColums(BYTE *block, int mode);
+BYTE* addRoundKey(BYTE *block, BYTE *rKey);
