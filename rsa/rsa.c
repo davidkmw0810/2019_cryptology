ac563a099f2c7cdf1fc667883e7d5cbd450003eb
Kim Minwoo <43179854+davidkmw0810@users.noreply.github.com>
2019-11-04 00:53:21 +0900

    RSA create

--- rsa/rsa.c
+++ rsa/rsa.c
@@ -12,7 +12,29 @@
 #include <time.h>
 #include "rsa.h"
 
+//#define "+" 43;
+//#define "-" 45;
+
 llint p, q, e, d, n;
+
+llint Mod(llint num, llint n){
+    if(num >= n)
+        return Mod(num - n, n);
+    else
+        return num;
+}
+
+llint Qu(llint a, llint b){
+
+    llint qu = 0;
+
+    while(a >= b){
+        a = a - b;
+        qu ++;
+    }
+
+    return qu;
+}
 
 /*
  * @brief     모듈러 덧셈 연산을 하는 함수.
@@ -24,7 +46,19 @@ llint p, q, e, d, n;
  * @todo      모듈러 값과 오버플로우 상황을 고려하여 작성한다.
  */
 llint ModAdd(llint a, llint b, byte op, llint n) {
-    return result
+
+    if(op == '+'){
+        if(a + b > n)
+            return Mod(a + b - n, n);
+        else
+            return Mod(a + b, n);
+    } else {
+        if(a - b > n)
+            return Mod(a - b - n, n);
+        else
+            return Mod(a - b, n);
+    }
+
 }
 
 /*
@@ -36,7 +70,16 @@ llint ModAdd(llint a, llint b, byte op, llint n) {
  * @todo       모듈러 값과 오버플로우 상황을 고려하여 작성한다.
  */
 llint ModMul(llint x, llint y, llint n) {
-    return result
+
+    if(y == 0){
+        return 0;
+    }
+    if(y - ((y >> 1) << 1) == 1){
+        return Mod(x + ModMul(x, y >> 1, n) + ModMul(x, y >> 1, n), n);
+    } else {
+        return Mod(ModMul(x, y >> 1, n) + ModMul(x, y >> 1, n), n);
+    }
+
 }
 
 /*
@@ -49,7 +92,15 @@ llint ModMul(llint x, llint y, llint n) {
                'square and multiply' 알고리즘을 사용하여 작성한다.
  */
 llint ModPow(llint base, llint exp, llint n) {
-    return result
+
+    if(exp == 0){
+        return base;
+    } else if(base - ((base >> 1) << 1) == 1){
+        return Mod(base*ModPow(base - 1, exp>>1, n), n);
+    } else if(base - ((base >> 1) << 1) == 0){
+        return Mod(ModPow(ModMul(base, base, n), exp>>1, n), n);
+    }
+
 }
 
 /*
@@ -58,10 +109,27 @@ llint ModPow(llint base, llint exp, llint n) {
  * @param      llint repeat    : 판단함수의 반복횟수.
  * @return     llint result    : 판단 결과에 따른 TRUE, FALSE 값.
  * @todo       Miller-Rabin 소수 판별법과 같은 확률적인 방법을 사용하여,
-               이론적으로 4N(99.99%) 이상 되는 값을 선택하도록 한다. 
+               이론적으로 4N(99.99%) 이상 되는 값을 선택하도록 한다.
  */
 bool IsPrime(llint testNum, llint repeat) {
-    return result
+
+    if(repeat == 0)
+        return TRUE;
+    llint s = testNum - 1;
+    llint d = s;
+    while(d - ((d >> 1) << 1) == 0) {d = d >> 1;}
+    llint a = (s-2)*WELLRNG512a() + 1;
+    llint r = (s-1)*WELLRNG512a();
+
+
+    if(ModPow(a, d, n) != 1){
+        for(int i = 0; i < r; i++){
+            if (!(ModPow(a, ModMul(ModPow(2, i, n), d, n), n) != n-1))
+                return IsPrime(testNum, repeat - 1);
+        }
+        return FALSE;
+    }
+
 }
 
 /*
@@ -72,7 +140,35 @@ bool IsPrime(llint testNum, llint repeat) {
  * @todo        확장 유클리드 알고리즘을 사용하여 작성하도록 한다.
  */
 llint ModInv(llint a, llint m) {
-    return result
+    llint s0 = 1, s1 = 0;
+    llint t0 = 0, t1 = 1;
+    llint r0 = a, r1 = m;
+    llint q0 = 0, q1 = Qu(r0, r1);
+    llint buffer;
+    llint n = 1;
+    while(TRUE){
+
+       buffer = r0;
+       r0 = r1;
+       r1 = Mod(buffer, r0);
+       if(r1 == 0) {break;}
+
+       buffer = q0;
+       q0 = q1;
+       q1 = Qu(r0, r1);
+
+       buffer = s0;
+       s0 = s1;
+       s1 = buffer - ModMul(s0, q0, m);
+
+       buffer = t0;
+       t0 = t1;
+       t1 = buffer - ModMul(t0, q0, m);
+
+       n++;
+    }
+
+    return s1;
 }
 
 /*
@@ -86,6 +182,7 @@ llint ModInv(llint a, llint m) {
  * @todo      과제 안내 문서의 제한사항을 참고하여 작성한다.
  */
 void miniRSAKeygen(llint *p, llint *q, llint *e, llint *d, llint *n) {
+
 }
 
 /*
@@ -97,7 +194,7 @@ void miniRSAKeygen(llint *p, llint *q, llint *e, llint *d, llint *n) {
  * @todo      과제 안내 문서의 제한사항을 참고하여 작성한다.
  */
 llint miniRSA(llint data, llint key, llint n) {
-    return result;
+    return 0;
 }
 
 llint GCD(llint a, llint b) {
@@ -115,6 +212,7 @@ llint GCD(llint a, llint b) {
 }
 
 int main(int argc, char* argv[]) {
+    /*
     byte plain_text[4] = {0x12, 0x34, 0x56, 0x78};
     llint plain_data, encrpyted_data, decrpyted_data;
     uint seed = time(NULL);
@@ -128,20 +226,29 @@ int main(int argc, char* argv[]) {
     // RSA 키 생성
     miniRSAKeygen(&p, &q, &e, &d, &n);
     printf("0. Key generation is Success!\n ");
-    printf("p : %lld\n q : %lld\n e : %lld\n d : %lld\n N : %lld\n\n", p, q, e, d, n);
+    printf("p : %llu\n q : %llu\n e : %llu\n d : %llu\n N : %llu\n\n", p, q, e, d, n);
 
     // RSA 암호화 테스트
     encrpyted_data = miniRSA(plain_data, e, n);
-    printf("1. plain text : %lld\n", plain_data);    
-    printf("2. encrypted plain text : %lld\n\n", encrpyted_data);
+    printf("1. plain text : %llu\n", plain_data);
+    printf("2. encrypted plain text : %llu\n\n", encrpyted_data);
 
     // RSA 복호화 테스트
     decrpyted_data = miniRSA(encrpyted_data, d, n);
-    printf("3. cipher text : %lld\n", encrpyted_data);
-    printf("4. Decrypted plain text : %lld\n\n", decrpyted_data);
+    printf("3. cipher text : %llu\n", encrpyted_data);
+    printf("4. Decrypted plain text : %llu\n\n", decrpyted_data);
 
     // 결과 출력
     printf("RSA Decryption: %s\n", (decrpyted_data == plain_data) ? "SUCCESS!" : "FAILURE!");
+
+
+    printf("%lf", WELLRNG512a());
+    */
+    llint a = 3;
+    llint b = 7;
+    llint c = WELLRNG512a()*100;
+    llint m = ModInv(a, b);
+    printf("%llu, %llu, %llu, %llu \n", a, b, c, m);
 
     return 0;
 }
